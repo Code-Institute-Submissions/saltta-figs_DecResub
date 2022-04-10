808028781eac9f216ec1b493d86e64227a56be79
saltta <trottsal@gmail.com>
2022-04-10 08:42:08 +0000

    added post comment method

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ class RecipeList(generic.ListView):
 
 # methods to display recipes and receive comments
 class RecipeDetail(View):
-
+    # display recipes
     def get(self, request, slug, *args, **kwargs):
         queryset = Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
@@ -29,6 +29,39 @@ class RecipeDetail(View):
             {
                 'recipe': recipe,
                 'comments': comments,
+                'commented': False,
+                'liked': liked,
+                'comment_form': CommentForm(),
+            },
+        )
+
+    # post comments
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False) 
+            comment.recipe = recipe
+            comment.save()
+        else:
+            comment_form = CommentForm()
+        
+        return render(
+            request,
+            'recipe_detail.html',
+            {
+                'recipe': recipe,
+                'comments': comments,
+                'commented': True,
                 'liked': liked,
                 'comment_form': CommentForm(),
             },
