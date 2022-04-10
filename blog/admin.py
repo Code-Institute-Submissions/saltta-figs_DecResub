e4cbbb713cf1073b9b248a2bbcb7343d8ff07602
saltta <trottsal@gmail.com>
2022-04-10 04:35:46 +0000

    recipe content field now has summernote functions

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Recipe)
+
+# makes the content field a summernote field
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
