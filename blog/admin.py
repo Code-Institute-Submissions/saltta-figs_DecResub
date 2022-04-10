7f2a4ac96459da65a4deeee1583e8f9bd956b55e
saltta <trottsal@gmail.com>
2022-04-10 04:45:01 +0000

    added functionality to recipe admin

--- blog/admin.py
+++ blog/admin.py
@@ -7,5 +7,9 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
 
